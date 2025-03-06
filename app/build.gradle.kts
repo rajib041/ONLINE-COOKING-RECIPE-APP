6455389145247702df1c8204363f10ca3959418e
rajib041 <rajibmicrosoft04@gmail.com>
2025-03-06 16:31:34 +0530

    HOME PAGE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidgif)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
--- app/src/main/java/com/hello/online_cooking_recipe_app/HomeActivity.kt
+++ app/src/main/java/com/hello/online_cooking_recipe_app/HomeActivity.kt
@@ -9,13 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        Handler(Looper.getMainLooper())
-            .postDelayed({startActivity(Intent(this,HomeActivity::class.java))
-                         finish()},2000)
+        setContentView(R.layout.activity_home)
+
     }
 }
